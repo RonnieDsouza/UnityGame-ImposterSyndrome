ec78cec43061dd6c0ef38952ea4ad2862044bb44
Ronnie D'Souza <ron99ds@gmail.com>
2022-02-20 09:37:31 -0600

    Added Player and Ground
    Player
    - Temporary Sprite
    - Basic XY movement
    
    Ground
    - Temporary Sprite

---
+++ ImposterSyndrome/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private float speed = 5.5F; // Comfortable Goldilocks speed
+    
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update X-axis position
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // Jump when Space is pressed
+        // TODO allow for other keys?
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+
+
+    }
+}
