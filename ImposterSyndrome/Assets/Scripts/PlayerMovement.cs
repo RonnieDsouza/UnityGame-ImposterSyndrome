999d769f451b3bc977e89beb176b064bdf0b4c10
Ronnie D'Souza <ron99ds@gmail.com>
2022-02-20 22:53:14 -0600

    Added Wall Jump, Updated Jumping
    Wall Jump
    - Player can now jump up and off walls.
    
    Update to Jumping
    - Rearranged the method to work with wall jumps
    - Uses Ray/Box Casting now to check if Grounded

--- ImposterSyndrome/Assets/Scripts/PlayerMovement.cs
+++ ImposterSyndrome/Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,29 @@
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-    private float speed = 5.5F; // Comfortable Goldilocks speed
+    private float speed = 5.5F; // Goldilocks speed
+    private float jumpHeight = 7F; // Goldilocks jump height
     private Animator anim;
-    private bool grounded;
-    
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    // private float jumpCooldown;
+    private float wallJumpCooldown;
+    float horizontalInput;
+
     private void Awake()
     {
         // Create References
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        
-    }
+        boxCollider = GetComponent<BoxCollider2D>();
+    } // Awake()
+
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Update X-axis position
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // flip player to face right or left based on Horizontal Input (Scale x3 for proper character size)
         if (horizontalInput > 0.01F)
@@ -29,27 +33,67 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01F)
             transform.localScale = new Vector3(-3,3,1);
 
-        // Jump when Space is pressed
-        // TODO allow for other keys?
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
+
+        if (wallJumpCooldown > 0.2F)
+        {
+            
+            // Update X-axis position
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+            if (onWall() && isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else body.gravityScale = 2.5F;
+
+            // Jump when Space is pressed
+            // TODO allow for other keys?
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+
+        } // if wallJumpCooldown
+        else wallJumpCooldown += Time.deltaTime;
+
 
         // Set Animation parameters for running and jumping
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
-    }
+        anim.SetBool("grounded", isGrounded());
+    } // Update()
+
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("jump");
-        grounded = false;
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            anim.SetTrigger("jump");
+        }
+        else if(onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x * 10), 0);
+                transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+            }
+            else
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x*3), 6);
+            wallJumpCooldown = 0;
+        }
+        
+    } // Jump()
 
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool isGrounded()
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
-    }
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1F, groundLayer);
+        return raycastHit.collider != null;
+    } // is Grounded
+
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1F, wallLayer);
+        return raycastHit.collider != null;
+    } // onWall()
 }
